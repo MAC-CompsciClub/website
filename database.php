3cb71ecd9a76d5a29658fd65240cefce0b2d2559
Hamza-S <hamzabman@gmail.com>
2016-11-24 01:08:36 -0500

    Connect to 'posts' database

--- database.php
+++ database.php
@@ -3,13 +3,13 @@
 $servername = "localhost";
 $username = "localhost";
 $password = "4ddbcTSRXWDHQjpS";
+$dbname = "posts";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 	if ($conn->connect_error){
 		die ("Connection failed: " . $conn->connect_error);
 		
 	}
-	echo "<p>Connected successfully</p>";
 
 ?>
