ce0ecbb006d5f611c38bc1a0a2e4e023e7e8f272
Hamza-S <hamzabman@gmail.com>
2016-11-23 23:41:21 -0500

    Create the nav bar div before anything else

--- admin_page.php
+++ admin_page.php
@@ -1,3 +1,9 @@
+<?php 
+	  
+	include 'nav_bar.php';
+  
+?>
+
 <?php
 	
 	session_start();
@@ -5,6 +11,7 @@
 	if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
 	{
 		echo "you're an admin";
+		
 	}
 	else{
 				
@@ -18,37 +25,15 @@
 <html lang = "en">
 	
 	<head>
-		<link rel="stylesheet" href="css/normalize.css">
-		<link rel="stylesheet" href="css/skeleton.css">
-		<link rel="stylesheet" href="css/custom.css">
-		<link rel="stylesheet" href="css/font.css">
 		<title>Welcome to the admin page</title>
 	</head>
 	
 	<body>
-		  <?php 
-	  
-			  include 'nav_bar.php';
-  
-  		?>
-
-
- <div class="container">
-    <section class="header">
-      <h1 class="title"> Admin Page </h1>
-    </section>
-    
-	<div class="docs-section">
-   	  <h2 align="center">Welcome to the Administrator Page!</h2>
-
-		<div align="center">
-			<form action="admin_logout.php">
+		
+		<h1 align="center">Admin page</h1>
+	<form align="center"action="admin_logout.php">
 	  		<input type="submit" value="Logout" />
-			</form>
-   		</div>
-
- 	</div>
+	  </form>
+	</body>
 	
-</body>
-
 </html>
