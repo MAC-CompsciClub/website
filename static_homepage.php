cef2aaf1d7784b2e077706822a3ac56a44be6cc9
Hamza-S <hamzabman@gmail.com>
2016-11-26 16:30:32 -0500

    Close sql connection after querys, add post counters, show posts on home-page by newest

--- static_homepage.php
+++ static_homepage.php
@@ -71,44 +71,43 @@
         <h2 align="center" class="docs-header">Welcome to the Computer Science club website for MAC!</h2>
 		
 		<?php
-        	$sql = "SELECT POSTID, postTitle, postDate, postAuthor, postBody FROM postdata";
+        	$sql = "SELECT POSTID, postTitle, postDate, postAuthor, postBody FROM postdata ORDER BY postDate DESC";
 	
 			$result = mysqli_query($conn, $sql);
+			
+				$postNum = 0;
 	
 				if (mysqli_num_rows($result) > 0){
 		
 					while ($row = mysqli_fetch_assoc($result)){
+						
+					$postNum += 1; //increment postNum so you can see what post # the post is
 					
 					$title = $row["postTitle"];
 					$timestamp = $row["postDate"];
 					$author = $row["postAuthor"];
 					$body = $row["postBody"];
 					
+					$postid = 'post-'.$postNum;
+					
 					
 					echo "
-						
-						<div class = 'post'>
-					
-						<h6 id = 'timestamp' align='right'>$timestamp</h6>
-						<h1 id = 'title'>$title</h1>
-						<h5 id = 'author'>by $author</h5> 
-						
-						<p id = 'postbody'>$body</p>
-						
-						
-					
-						
+						<div id = $postid class = 'post'>
+							<h6 id = 'timestamp' align='right'>$timestamp</h6>
+							<h1 id = 'title'>$title</h1>
+							<h5 id = 'author'>by $author</h5> 
+							<p id = 'postbody'>$body</p>
+							<br>$postid</br>
 						</div>
-					
-					
-					
-					";
+					   ";
 			
 					}
 				}
 				else{
-					echo "no results";
+					echo "<h5 align = 'center'>No posts</h5>";
 				}
+
+			$conn->close();
 		?>
     </div>
   
