3a65c2c5714aecae1c9c0b6044517b11482d48ba
Hamza-S <hamzabman@gmail.com>
2017-01-22 14:59:47 -0500

    Implement access_posts.php, clean up code

--- static_homepage.php
+++ static_homepage.php
@@ -1,115 +1,110 @@
 <?php 
-	  
-	require("sql/database.php");	
   
+require("sql/database.php");	
+
 ?>
 
 
 <?php
+
+session_start();
+
+
+if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
+{
 	
-	session_start();
+	$buttonText = "ADMIN PAGE";  //Variables for the button ID
+	$buttonRef = "admin_page.php";
 	
-	
-	if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true){
-		
-		$buttonText = "ADMIN PAGE";  //Variables for the button ID
-		$buttonRef = "admin_page.php";
-		
-	}
-	else{
-		$buttonText = "ADMIN LOGIN";
-		$buttonRef = "admin_login.php";
-	}
-	
+}
+else
+{
+	$buttonText = "ADMIN LOGIN";
+	$buttonRef = "admin_login.php";
+}
+
 ?>
 
 <!DOCTYPE html>
 
 <html lang="en">
 
-  <head>
+<head>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Mac Compsci Club</title>
-  <meta name="description" content="This is the main page for the Macdonald Computer Science Club.">
-  <meta name="author" content="Jack Wang + Hamza Saleem & The Compsci Club">
+<!-- Basic Page Needs
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta charset="utf-8">
+<title>Mac Compsci Club</title>
+<meta name="description" content="This is the main page for the Macdonald Computer Science Club.">
+<meta name="author" content="Jack Wang + Hamza Saleem & The Compsci Club">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Mobile Specific Metas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-  <link rel="stylesheet" href="css/custom.css">
-  <link rel="stylesheet" href="css/font.css">
-  
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
+<!-- CSS
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/skeleton.css">
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="css/font.css">
 
-  </head>
+<!-- Favicon
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="icon" type="image/png" href="images/favicon.png">
 
-  
+</head>
+
+
 <body class = "homepage">
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->	
-  <?php  
-	  
-	  include ('actions/nav_bar.php'); 
-	  
-  ?>
-  	<div>
-        <div class = "header">
-        <h1 align = "center" >Computer Science Club</h1>
-        <h2 align="center" class="docs-header">Welcome to the Computer Science club website for MAC!</h2>
-        </div>
-		<?php
-        	$sql = "SELECT POSTID, postTitle, postDate, postAuthor, postBody FROM postdata ORDER BY postDate DESC";
-	
-			$result = mysqli_query($conn, $sql);
-			
-				$postNum = 0;
-	
-				if (mysqli_num_rows($result) > 0){
-		
-					while ($row = mysqli_fetch_assoc($result)){
-						
-					$postNum += 1; //increment postNum so you can see what post # the post is
-					
-					$title = $row["postTitle"];
-					$timestamp = $row["postDate"];
-					$author = $row["postAuthor"];
-					$body = $row["postBody"];
-					
-					$postid = 'post-'.$postNum;
-					
-					
-					echo "
-						<div id = $postid class = 'post'>
-							<h6 id = 'timestamp' align='right'>$timestamp</h6>
-							<h1 id = 'title'>$title</h1>
-							<h5 id = 'author'>by $author</h5> 
-							<p id = 'postbody'>$body</p>
-							<br>$postid</br>
-						</div>
-					   ";
-			
-					}
-				}
-				else{
-					echo "<h5 align = 'center'>No posts</h5>";
-				}
-
-			$conn->close();
-		?>
-    </div>
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->	
+<?php  
   
+  include ('actions/nav_bar.php'); 
+  
+?>
+	<div>
+    <div class = "header">
+    <h1 align = "center" >Computer Science Club</h1>
+    <h2 align="center" class="docs-header">Welcome to the Computer Science club website for MAC!</h2>
+    </div>
+	<?php	
+		include ("actions/access_posts.php");
+			
+		if (count($posts) > 0)
+		{	
+			for ($i = 0; $i < count($posts); $i++)
+				{
+				$postid = 'post-'.$i;
+				
+				$title = $posts[$i][0];
+				$timestamp = $posts[$i][1];
+				$author = $posts[$i][2];
+				$body = $posts[$i][3];
+			
+				echo "
+				<div id = $postid class = 'post'>
+					<h6 id = 'timestamp' align='right'>$timestamp</h6>
+					<h1 id = 'title'>$title</h1>
+					<h5 id = 'author'>by $author</h5> 
+					<p id = 'postbody'>$body</p>
+					<br>$postid</br>
+				</div>
+				";
+			   }
+		
+				
+			}
+			else{
+				echo "<h5 align = 'center'>No posts</h5>";
+			}
+    	
+	?>
+</div>
+
 <!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 
 </html>
