f01f967f31cecb4e380bf0a90c8c2f82c7a2fcbd
Hamza-S <hamzabman@gmail.com>
2017-01-22 14:59:19 -0500

    Remove redirect, restrict access in .htaccess

--- sql/database.php
+++ sql/database.php
@@ -1,15 +1,16 @@
 <?php
+	
+		$servername = "localhost";
+		$username = "localhost";
+		$password = "4ddbcTSRXWDHQjpS";
+		$dbname = "posts";
 
-$servername = "localhost";
-$username = "localhost";
-$password = "4ddbcTSRXWDHQjpS";
-$dbname = "posts";
+		$conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-	if ($conn->connect_error){
-		die ("Connection failed: " . $conn->connect_error);
+		if ($conn->connect_error){
+			die ("Connection failed: " . $conn->connect_error);
 		
-	}
+		}
+		
 
 ?>
