7eb8d3a92544384fccb8c033b790cc7aab962ffd
Hamza-S <hamzabman@gmail.com>
2017-03-05 14:26:35 -0500

    File

---
+++ actions/new_post.php
@@ -0,0 +1,31 @@
+<?php
+	
+	require ("../sql/database.php"); //conncet to db
+		
+	$title = $_POST["ptitle"];
+	$author = $_POST["pauthor"];
+	$body = $_POST["pbody"];
+	
+	//$title = mysqli_real_escape_string($title);
+	//$author = mysqli_real_escape_string($author);
+	//$body = mysqli_real_escape_string($body);
+	
+	$sql = "INSERT INTO postdata (POSTID, postTitle, postDate, postAuthor, postBody) VALUES
+            (NULL, '$title', CURRENT_TIMESTAMP, '$author', '$body');";
+		if(mysqli_query($conn, $sql))
+		{
+			echo "Records added successfully.";
+		} 
+		else
+		{
+			echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+		}
+	
+	
+	$conn->close();
+	
+	header("Location: ../admin/admin_page.php");
+	die();
+		
+	
+?>
