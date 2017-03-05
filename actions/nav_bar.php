e4681c9d155029a9071c2f777093564bb5340910
Hamza-S <hamzabman@gmail.com>
2017-03-05 14:06:02 -0500

    Directory

--- actions/nav_bar.php
+++ actions/nav_bar.php
@@ -6,10 +6,10 @@
 <html>
 	<head>
 		
-		 <link rel="stylesheet" href="/site/css/custom.css"/> <!--Path must be absolute!-->
-		   <link rel="stylesheet" href="/site/css/normalize.css">
-		   <link rel="stylesheet" href="/site/css/skeleton.css">
-		   <link rel="stylesheet" href="/site/css/font.css">
+		 <link rel="stylesheet" href="/website/css/custom.css"/> <!--Path must be absolute!-->
+		   <link rel="stylesheet" href="/website/css/normalize.css">
+		   <link rel="stylesheet" href="/website/css/skeleton.css">
+		   <link rel="stylesheet" href="/website/css/font.css">
 		   
 		 
 	</head>
@@ -17,19 +17,19 @@
 	
 	<nav>
 		<ul class = "navbar">
-		<li><a href = "/site/static_homepage.php">Home</a></li>
-		<li><a href = "/site/static_homepage.php">Resources</a></li>
+		<li><a href = "/website/static_homepage.php">Home</a></li>
+		<li><a href = "/website/static_homepage.php">Resources</a></li>
 				
 			<?php if (!isset($_SESSION['isAdmin']))
 				{
-					echo "<li><a href = '/site/admin/admin_login.php'>Admin Login</a></li>"; //Only make this button 									visible if the user is an admin
+					echo "<li><a href = '/website/admin/admin_login.php'>Admin Login</a></li>"; //Only make this button 									visible if the user is an admin
 				}
 			?>
 				
 				
 			<?php if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true)
 				{
-					echo "<li><a href = '/site/admin/admin_page.php'>Admin Page</a></li>"; //Only make this button 															visible if the user is an admin
+					echo "<li><a href = '/website/admin/admin_page.php'>Admin Page</a></li>"; //Only make this button 															visible if the user is an admin
 				}
 				?>
 		</ul>
